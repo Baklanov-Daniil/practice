043650121a56a9e7109a23e4fe102641e1d5001d
Baklanov-Daniil <daniilbaklanov55@gmail.com>
2025-06-23 16:01:17 +0600

    feat: realisation

--- task01/Class1.cs
+++ task01/Class1.cs
@@ -1,6 +1,23 @@
-﻿namespace task01;
-
-public class Class1
+﻿using System;
+public static class StringExtensions
 {
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return false;
 
+        string cleaned = "";
+
+        foreach (char c in input.ToLower())
+        {
+            if (!char.IsWhiteSpace(c) && !char.IsPunctuation(c))
+                cleaned += c;
+        }
+
+        char[] chars = cleaned.ToCharArray();
+        Array.Reverse(chars);
+        string reversed = new string(chars);
+        
+        return cleaned == reversed;
+    }
 }
