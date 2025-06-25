78777e87a8f3262a7e4af029a47c87a3676e6cd6
Baklanov-Daniil <daniilbaklanov55@gmail.com>
2025-06-25 18:55:50 +0600

    init+realization

---
+++ task05/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+public class ClassAnalyzer
+{
+    private Type _type;
+
+    public ClassAnalyzer(Type type)
+    {
+        _type = type;
+    }
+
+    public IEnumerable<string> GetPublicMethods()
+        => _type.GetMethods().Select(m => m.Name);
+    public IEnumerable<string> GetMethodParams(string methodname)
+        => _type.GetMethod(methodname)
+            .GetParameters().Select(p => p.Name);
+
+    public IEnumerable<string> GetAllFields()
+        => _type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                      .Select(p => p.Name);
+
+    public IEnumerable<string> GetProperties()
+        => _type.GetProperties().Select(p => p.Name);
+    public bool HasAttribute<T>() where T : Attribute
+        => _type.GetCustomAttribute<T>() != null;
+}
