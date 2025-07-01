875385edb16c2b6610f00e66137bac65e5301090
Baklanov-Daniil <daniilbaklanov55@gmail.com>
2025-07-01 16:35:33 +0600

    fix: test realization

---
+++ CommandRunner/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var assembly = Assembly.LoadFrom("FileSystemCommands.dll");
+
+        var sizeCmdType = assembly.GetType("FileSystemCommands.DirectorySizeCommand");
+        var sizeCmd = (ICommand)Activator.CreateInstance(sizeCmdType, new object[] { @"C:\Test" });
+        sizeCmd.Execute();
+
+        var findCmdType = assembly.GetType("FileSystemCommands.FindFilesCommand");
+        var findCmd = (ICommand)Activator.CreateInstance(findCmdType, new object[] { @"C:\Test", "*.txt" });
+        findCmd.Execute();
+    }
+}
---
+++ FileSystemCommands/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+
+public class DirectorySizeCommand : ICommand
+{
+    private string _path {get; }
+
+    public long size = 0;
+
+    public DirectorySizeCommand(string path)
+    {
+        _path = path;
+    }
+
+    public void Execute()
+    {
+        if (!Directory.Exists(_path))
+        {
+            return;
+        }
+        this.size = CalculateSize(_path);
+    }
+
+    private long CalculateSize(string directory)
+    {
+        long size = 0;
+
+        foreach (var file in Directory.GetFiles(directory))
+            size += new FileInfo(file).Length;
+
+        foreach (var dir in Directory.GetDirectories(directory))
+            size += CalculateSize(dir);
+
+        return size;
+    }
+}
+
+public class FindFilesCommand : ICommand
+{
+    private string _path { get; }
+    private string _pattern { get; }
+
+    public string[] Files = Array.Empty<string>();
+
+    public FindFilesCommand(string path, string pattern)
+    {
+        _path = path;
+        _pattern = pattern;
+    }
+
+    public void Execute()
+    {
+        if (!Directory.Exists(_path))
+        {
+            return;
+        }
+        this.Files = Directory.GetFiles(_path, _pattern, SearchOption.AllDirectories);
+    }
+}
