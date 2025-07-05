16d0e3ea3511ac9c220083cc6283edb3211a0ecc
Baklanov-Daniil <daniilbaklanov55@gmail.com>
2025-07-05 17:24:21 +0600

    style: new line

---
+++ task11/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+public interface ICalculator
+{
+    int Add(int a, int b);
+    int Subtract(int a, int b);
+    int Multiply(int a, int b);
+    int Divide(int a, int b);
+}
+
+public class CalculatorGenerator
+{
+    public static ICalculator CreateCalculator()
+    {
+        string code = @"
+            public class DynamicCalculator : ICalculator
+            {
+                public int Add(int a, int b) => a + b;
+                public int Subtract(int a, int b) => a - b;
+                public int Multiply(int a, int b) => a * b;
+                public int Divide(int a, int b) => a / b;
+            }";
+
+        var references = new MetadataReference[]
+        {
+            MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+            MetadataReference.CreateFromFile(typeof(ICalculator).Assembly.Location),
+        };
+
+        var compilation = CSharpCompilation.Create(
+            assemblyName: "CalculatorAssembly",
+            syntaxTrees: new[] { CSharpSyntaxTree.ParseText(code) },
+            references: references,
+            options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+        using var memoryStream = new MemoryStream();
+        var result = compilation.Emit(memoryStream);
+
+        memoryStream.Seek(0, SeekOrigin.Begin);
+        var assembly = Assembly.Load(memoryStream.ToArray());
+        var calculatorType = assembly.GetType("DynamicCalculator");
+
+        if (calculatorType == null)
+            throw new Exception("Класс не создан");
+
+        return (ICalculator)Activator.CreateInstance(calculatorType)!;
+    }
+}
---
+++ task11test/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using Xunit;
+using System;
+
+public class CalculatorTests
+{
+    private readonly dynamic _calculator;
+
+    public CalculatorTests()
+    {
+        _calculator = CalculatorGenerator.CreateCalculator();
+    }
+
+    [Fact]
+    public void Add_ShouldReturnCorrectSum()
+    {
+        int a = 6, b = 3;
+
+        int result = _calculator.Add(a, b);
+
+        Assert.Equal(9, result);
+    }
+
+    [Fact]
+    public void Subtract_ShouldReturnCorrectDifference()
+    {
+        int a = 6, b = 3;
+
+        int result = _calculator.Subtract(a, b);
+
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void Multiply_ShouldReturnCorrectProduct()
+    {
+        int a = 6, b = 3;
+
+        int result = _calculator.Multiply(a, b);
+
+        Assert.Equal(18, result);
+    }
+
+    [Fact]
+    public void Divide_ShouldReturnCorrectQuotient()
+    {
+        int a = 6, b = 3;
+
+        int result = _calculator.Divide(a, b);
+        Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void Divide_ByZero_ShouldThrowDivideByZeroException()
+    {
+        int a = 5, b = 0;
+        Assert.Throws<DivideByZeroException>(() => _calculator.Divide(a, b));
+    }
+
+    [Fact]
+    public void Calculator_Instance_IsCreatedSuccessfully()
+    {
+        var calculator = CalculatorGenerator.CreateCalculator();
+        Assert.NotNull(calculator);
+        Assert.IsAssignableFrom<ICalculator>(calculator);
+    }
+}
